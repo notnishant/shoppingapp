ac26e409bcca12bc67993f0bf55e200c365d2851
Notnishant <nishant777000777@gmail.com>
2021-07-01 01:24:23 +0530

    commit

---
+++ lib/screens/user_info.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Userinfo extends StatelessWidget {
+  const Userinfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('UserInfo'),
+      ),
+    );
+  }
+}
